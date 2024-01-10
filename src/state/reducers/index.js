b3521a75f71859cb6d18a327e52385d6d6a33330
Art Vardanyan <akvardanyan99@gmail.com>
2024-01-10 11:19:48 -0500

    combine reducers

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import vizReducer from './vizReducer';
+import fetchFilterReducer from './fetchFilterReducer';
+import dataReducer from './dataReducer';
 
 export default combineReducers({
   vizReducer,
+  fetchFilterReducer,
+  dataReducer,
 });
