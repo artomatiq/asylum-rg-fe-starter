7f8ae65d2e0527dfb045cde6d7bc85b13027d0dc
Art Vardanyan <akvardanyan99@gmail.com>
2024-01-19 14:32:17 -0500

    wip

---
+++ src/auth0Provider.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const history = useHistory();
+
+  const onRedirectCallback = appState => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
+
+//it uses the Auth0Provider that comes from the SDK. It's gonna create a provider around that.
+//And then we're gonna tell it what the configurations are.
+
+//the main thing we're trying to do here is use useHistory from React Router to attach it to this provider
