9be9bb89e4f26436fd13be8c9906f24d119698a9
Art Vardanyan <akvardanyan99@gmail.com>
2024-01-23 21:39:13 -0500

    done?

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+import '../../../styles/RenderLandingPage.less';
 
-export default function ProfilePage() {
+const ProfilePage = () => {
   const { user, isLoading } = useAuth0();
   const { name, picture, email } = user;
 
   if (isLoading) return null;
 
   return (
-    <div>
+    <div
+      style={{
+        fontFamily: 'acumin',
+        margin: '12vh',
+        paddingLeft: '5vh',
+        borderLeft: '5px double black',
+      }}
+    >
       <img
         src={picture}
         alt="User"
-        style={{ marginLeft: '10vh', marginTop: '10vh', marginBottom: '9vh' }}
+        style={{ marginTop: '10vh', marginBottom: '6vh', borderRadius: '15%' }}
       />
-      <h1 style={{ marginLeft: '10vh' }}>Name: {name}</h1>
-      <h2 style={{ marginLeft: '10vh', marginBottom: '10vh' }}>
-        Email: {email}
-      </h2>
+      <h1 style={{}}>Name: {name}</h1>
+      <h2 style={{ marginBottom: '10vh' }}>Email: {email}</h2>
     </div>
   );
-}
+};
+
+export default withAuthenticationRequired(ProfilePage);
