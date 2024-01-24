96ac88c284fc9d7bf33c6e5fae778ceeaee52d72
Art Vardanyan <akvardanyan99@gmail.com>
2024-01-23 21:44:23 -0500

    done?

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+const h = null;
 
 const ProfilePage = () => {
   const { user, isLoading } = useAuth0();
